108a3d81cc65c94bf2badef36966c820557a50e1
sukui01 <121461709+sukui01@users.noreply.github.com>
2022-12-26 16:55:22 -0500

    Create 1.1.py

---
+++ In_Class/1.1.py
@@ -0,0 +1,10 @@
+'''
+Write code to display days of weeks on separate lines using a single print statement.
+'''
+
+
+# assign the list with day to the variable 
+weekdays = ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+
+for day in weekdays:
+    print(day)
