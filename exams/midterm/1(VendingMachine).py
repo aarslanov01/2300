04d89fcf5c091b883f3f275f36685c711ecb3e67
Ainur Arslanov <121461709+sukui01@users.noreply.github.com>
2022-12-26 18:23:40 -0500

    Update 1(VendingMachine).py

--- exams/midterm/1(VendingMachine).py
+++ exams/midterm/1(VendingMachine).py
@@ -1,13 +1,6 @@
-'''
-Write a program for a vending machine that allows the user to insert up to $5 in coins and bills. 
-While the user is entering money, display the total amount entered to the user. When they have completed entering money, 
-display the menu of available options in the vending machine (minimum 3) and their prices. The user may choose as many items as their money allows, 
-and after each selection they should be allowed to end their machine usage and have their remaining money returned.
-'''
-
 # count the money
 total_amount = 0
-# range constrain
+# range constraint
 amount_range = range(0,6)
 # ask the amount
 amount = float(input('please insert the money (up to $5): '))
@@ -19,9 +12,10 @@ if amount in amount_range:
     
 else:
     amount = float(input('please insert the money (up to $5): '))
-
+   
+    
     #print the menu
-print('1: Soda, Price: $3')
+print('\n1: Soda, Price: $3')
 print('2: Ice Cream, Price: $5')
 print('3: Pretzels, Price: $2')
 
@@ -31,23 +25,24 @@ choice = int(input('please choose an item: '))
 while total_amount >0: 
     if choice == 1:
         total_amount -= 3
-        print(f'you have left ${amount}')
+        print(f'\nyou have left ${total_amount}')
         print(f'please grab your {total_amount} of change')
+        break
 
     elif choice == 2:
         total_amount -= 5
-        print(f'you have left ${amount}')
+        print(f'\nyou have left ${total_amount}')
         print(f'please grab your {total_amount} of change')
+        break
     
     elif choice == 3:
         total_amount -= 2
-        print(f'you have left ${amount}')
+        print(f'\nyou have left ${total_amount}')
         print(f'please grab your {total_amount} of change')
+        break
 
         #break if we dont have money
 if total_amount == 0:
-    print('you dont have any money')
+    print('\nyou dont have any money')
     print('please insert more money to buy something else')
-
-
 
